3e919e5577402218d2b8d7816fcc51e84fb3cab1
Gabriel-Lemus <glemus.stuart@gmail.com>
2020-10-26 21:16:18 -0600

    Exposed SHA256 function

--- public/dist/hash.js
+++ public/dist/hash.js
@@ -27596,6 +27596,10 @@ function config (name) {
 },{"./core":188}],190:[function(require,module,exports){
 let SHA256 = require('crypto-js/sha256');
 
+window.sha256 = function (data) {
+  return SHA256(data).toString();
+}
+
 let input = document.getElementById('data-input');
 let hash = document.getElementById('hash-representation');
 
--- public/scripts/hash.js
+++ public/scripts/hash.js
@@ -1,5 +1,9 @@
 let SHA256 = require('crypto-js/sha256');
 
+window.sha256 = function (data) {
+  return SHA256(data).toString();
+}
+
 let input = document.getElementById('data-input');
 let hash = document.getElementById('hash-representation');
 
